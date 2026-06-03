82f62404a58fe7ee86c794da8609f3b2b510a918
shuvo794 <shuvohossain794@gmail.com>
2026-06-03 11:32:28 +0600

    Add CORS middleware for cross-origin resource sharing

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require( 'express' );
 const app = express();
+const cors = require( 'cors' );
+app.use( cors() );
 const port = process.env.PORT || 3000;
 const users = [
     { id: 1, name: 'John Doe', email: 'john.doe@example.com' },
