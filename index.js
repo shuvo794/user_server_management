9e7dea65be9fdcf38ffa4d37622a37f8cfa5eaab
shuvo794 <shuvohossain794@gmail.com>
2026-06-03 14:16:33 +0600

    Implement user creation endpoint and ensure JSON body parsing

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require( 'express' );
 const app = express();
 const cors = require( 'cors' );
 app.use( cors() );
+app.use( express.json() );
 const port = process.env.PORT || 3000;
 const users = [
     { id: 1, name: 'John Doe', email: 'john.doe@example.com' },
@@ -11,6 +12,13 @@ const users = [
 app.get( '/users', ( req, res ) =>
 {
     res.json( users );
+} );
+app.post( '/users', ( req, res ) =>
+{
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push( newUser );
+    res.status( 201 ).json( newUser );
 } );
 app.get( '/', ( req, res ) =>
 {
